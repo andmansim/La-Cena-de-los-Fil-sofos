69eca0f9b451984fe79a47a83f772c63fc0ea3f6
andmansim <91722324+andmansim@users.noreply.github.com>
2023-04-05 17:50:04 +0200

    inicio

--- gui.py
+++ gui.py
@@ -1,16 +1,14 @@
-import tkinter as tk
+from tkinter import*
 
-'''
-class Ventana(tk.Frame):
-    def __init__(self, master = None):
-        super(Ventana, self).__init__(master, width=1050, height=630)
-        self.master.title('La Cena de los Filósofos')
-        
-        self.pack()
-ventana = Ventana()
-ventana.mainloop()'''
 
-ventana = tk.Tk()
-bot = tk.Button(ventana, text = 'NS', width = 25)
-bot.grid(column=0, row = 0, padx = 5, pady=5 ) #posición 
+
+ventana = Tk()
+
+ventana.geometry('1050x630')
+etiqueta = Label(ventana, text='La cena de los filósofos')
+etiqueta.pack()
+
+
+
+
 ventana.mainloop()
