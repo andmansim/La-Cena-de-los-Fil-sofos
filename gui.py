414fc7ffcc394a81ef7381edb0d534d4cce7599e
andmansim <91722324+andmansim@users.noreply.github.com>
2023-04-05 19:57:12 +0200

    me esty cansando de estamierda

--- gui.py
+++ gui.py
@@ -1,7 +1,6 @@
 from tkinter import*
 
 
-
 ventana = Tk()
 
 #ventana.geometry('1050x630')
@@ -20,13 +19,20 @@ def numeros(f, c, color):
     n.grid(row=f, column=c)
     n.config(width='15', height='15')
     n.config(bg=color)
-    
-filo0 = cajas_filo(4, 4, 'pink')
 
+def texto(f, c, palabra, dato) :
+    
+    dato = Label(ventana, text = palabra).grid(row=f, column=c)
+filo0 = cajas_filo(4, 4, 'pink')
+texto(4, 4, 'Filósofo 1', filo0)
 filo5 = cajas_filo(6, 2, 'yellow')
+texto(6, 2, 'Filósofo 5', filo5)
 filo3 = cajas_filo(8, 3, 'yellow')
+texto(8, 3, 'Filósofo 3', filo3)
 filo2 = cajas_filo(6, 6, 'white')
+texto(6, 6, 'Filósofo 2', filo2)
 filo4 = cajas_filo(8, 5, 'pink')
+texto(8, 5, 'Filósofo 4', filo4)
 
 uno = numeros(4, 3, 'blue')
 dos = numeros(5,5,'gray')
@@ -35,6 +41,9 @@ cuatro= numeros(8, 4, 'blue')
 cinco= numeros(7, 2, 'blue')
 #marco_principal.config(width='1050', height='630')
 
-botsalir = Button(ventana, text='Salir').grid(sticky= S+W)
-botcreditos = Button(ventana, text='Créditos').grid(sticky= E+W)
+botsalir = Button(ventana, text='Salir')
+botcreditos = Button(ventana, text='Créditos').grid(sticky= E)
+botpausar = Button(ventana, text='Pausar').grid(sticky= E)
+botiniciar= Button(ventana, text='Iniciar').grid(sticky= E)
+botresert = Button(ventana, text='Resert').grid(sticky= E)
 ventana.mainloop()
