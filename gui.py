8a11d41b4958ab499299b4a88f811f7083d222a1
andmansim <91722324+andmansim@users.noreply.github.com>
2023-04-05 18:27:16 +0200

    numeros

--- gui.py
+++ gui.py
@@ -8,30 +8,24 @@ ventana = Tk()
 etiqueta = Label(ventana, text='La cena de los filósofos')
 etiqueta.grid(column=1, row=0, pady = 5)
 
-#filosofo 1
-filo1 = Frame()
-filo1.grid(row=4, column=6)
-filo1.config(width='100', height='30')
-filo1.config(bg='pink')
-
-
-#filosofo 2
-filo2 = Frame()
-filo2.grid(row=4, column=5)
-filo2.config(width='100', height='30')
-filo2.config(bg='white')
 
 def cajas_filo(f,c, color):
     filo = Frame()
-    filo.grid(row=f, column=f)
+    filo.grid(row=f, column=c)
     filo.config(width='100', height='30')
     filo.config(bg=color)
     
-filo= cajas_filo(4, 4, 'white')
-filo0 = cajas_filo(4, 5, 'pink')
-filo5 = cajas_filo(5, 3, 'yellow')
-filoo = cajas_filo(5, 5, 'white')
-filo2 = cajas_filo(5, 6, 'gray')
+def numeros(f, c, color):
+    n =Frame()
+    n.grid(row=f, column=c)
+    n.config(width='10', height='10')
+    n.config(bg=color)
+    
+filo0 = cajas_filo(4, 4, 'pink')
+filo5 = cajas_filo(6, 2, 'yellow')
+filo3 = cajas_filo(8, 3, 'yellow')
+filo2 = cajas_filo(6, 6, 'gray')
+filo4 = cajas_filo(8, 5, 'pink')
 #marco_principal.config(width='1050', height='630')
 
 ventana.mainloop()
