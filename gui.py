90b0041089c9204638174ca80de80bfb187df035
andmansim <91722324+andmansim@users.noreply.github.com>
2023-04-05 18:16:15 +0200

    cajas

--- gui.py
+++ gui.py
@@ -15,8 +15,23 @@ filo1.config(width='100', height='30')
 filo1.config(bg='pink')
 
 
+#filosofo 2
+filo2 = Frame()
+filo2.grid(row=4, column=5)
+filo2.config(width='100', height='30')
+filo2.config(bg='white')
 
-
+def cajas_filo(f,c, color):
+    filo = Frame()
+    filo.grid(row=f, column=f)
+    filo.config(width='100', height='30')
+    filo.config(bg=color)
+    
+filo= cajas_filo(4, 4, 'white')
+filo0 = cajas_filo(4, 5, 'pink')
+filo5 = cajas_filo(5, 3, 'yellow')
+filoo = cajas_filo(5, 5, 'white')
+filo2 = cajas_filo(5, 6, 'gray')
 #marco_principal.config(width='1050', height='630')
 
 ventana.mainloop()
