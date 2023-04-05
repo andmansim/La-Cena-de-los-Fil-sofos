43a94b896eff7fd8c2949321b98378d81a801ed4
andmansim <91722324+andmansim@users.noreply.github.com>
2023-04-05 18:09:20 +0200

    cuadros

--- gui.py
+++ gui.py
@@ -4,11 +4,19 @@ from tkinter import*
 
 ventana = Tk()
 
-ventana.geometry('1050x630')
+#ventana.geometry('1050x630')
 etiqueta = Label(ventana, text='La cena de los filósofos')
-etiqueta.pack()
+etiqueta.grid(column=1, row=0, pady = 5)
+
+#filosofo 1
+filo1 = Frame()
+filo1.grid(row=4, column=6)
+filo1.config(width='100', height='30')
+filo1.config(bg='pink')
 
 
 
+
+#marco_principal.config(width='1050', height='630')
 
 ventana.mainloop()
