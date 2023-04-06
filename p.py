52852e5e191c302d3a1e1854ecfc79c3caeb1e9f
andmansim <91722324+andmansim@users.noreply.github.com>
2023-04-06 11:57:01 +0200

    ya

--- p.py
+++ p.py
@@ -5,7 +5,6 @@ from tkinter import scrolledtext as st
 class Aplicacion:
     def __init__(self):
         self.ventana1=tk.Tk()
-
         self.framecopia()        
         self.ventana1.mainloop()
 
@@ -13,13 +12,11 @@ class Aplicacion:
     def framecopia(self):
         self.labelframe1=ttk.LabelFrame(self.ventana1, text="Region")
         self.labelframe1.grid(column=0, row=1, padx=5, pady=5, sticky="w")
-        
-      
-
-        
-
-
-    
+        self.label1=ttk.Label(self.labelframe1, text="Desde fila:")
+        self.label1.grid(column=0, row=0, padx=5, pady=5, sticky="e")
+        self.dato1=tk.StringVar()
+        self.entry1=ttk.Entry(self.labelframe1, textvariable=self.dato1)
+        self.entry1.grid(column=1, row=0, padx=5, pady=5, sticky="e")
         
 
 aplicacion1=Aplicacion() 
